3e3ce57ad8887cd7b6f93a000a24920302b0b523
YouZhou <zhouyou0528@sina.com>
2019-04-08 15:13:45 -0400

    v1.0.0

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const path = require('path');
 const app = express();
 const port =  process.env.PORT || 5000;
 
@@ -18,5 +19,14 @@ mongoose.connect(db, { useNewUrlParser: true })
     
 //use routes
 app.use('/api/items', items);
+
+//Serve static assets if in production
+if(process.env.NODE_ENV === 'production') {
+// Set static folder 
+    app.use(express.static('client/build'));
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    })
+}
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
