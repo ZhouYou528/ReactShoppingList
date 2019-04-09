12979ee0c31c4ca6ecbb7fc552c2b2a940f754cb
ZhouYou528 <zhouyou0528@sina.com>
2019-04-09 08:49:32 +0800

    user auth added

--- config/keys.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-    mongoURI: 'mongodb://zhouyou0528:zhouyou0528@ds121238.mlab.com:21238/reactshoppinglist'
-}
--- server.js
+++ server.js
@@ -1,24 +1,32 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const path = require('path');
 const app = express();
 const port =  process.env.PORT || 5000;
+const config = require('config');
 
 const items = require('./routes/api/items');
+const users = require('./routes/api/users');
+const auth = require('./routes/api/auth');
 
-app.use(bodyParser.json());
+app.use(express.json());
 
 //DB config
-const db = require('./config/keys').mongoURI;
+const db = config.get('mongoURI');
 
 //connect to Mongo
-mongoose.connect(db, { useNewUrlParser: true })
+mongoose
+    .connect(db, { 
+        useNewUrlParser: true,
+        useCreateIndex: true 
+    })
     .then(() => console.log('Database connected!'))
     .catch(err => console.log(err));
     
 //use routes
 app.use('/api/items', items);
+app.use('/api/users', users);
+app.use('/api/auth', auth);
 
 //Serve static assets if in production
 if(process.env.NODE_ENV === 'production') {
