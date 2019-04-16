e433ce6a8713e7fc4b63feeeb2b2d6897c79f533
YouZhou <zhouyou0528@sina.com>
2019-04-16 17:52:02 -0400

    refresh will keep user session

--- client/src/components/AppNavbar.js
+++ client/src/components/AppNavbar.js
@@ -1,82 +1,90 @@
 import React, { Component, Fragment } from 'react';
 import RegisterModal from './auth/RegisterModal';
 import {
-    Collapse,
-    Navbar,
-    NavbarToggler,
-    NavbarBrand,
-    Nav,
-    NavItem,
-    Container
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  Container
 } from 'reactstrap';
 import Logout from './auth/Logout';
 import Login from './auth/LoginModal';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import { loadUser } from '../actions/authAction';
 
 class AppNavbar extends Component {
-    state = {
-        isOpen: false
-    }
+  state = {
+    isOpen: false
+  };
 
-    static propTypes = {
-        auth: PropTypes.object.isRequired
-    }
+  componentDidMount() {
+    this.props.loadUser();
+  }
+  static propTypes = {
+    auth: PropTypes.object.isRequired,
+    loadUser: PropTypes.func.isRequired
+  };
 
-    toggle = () => {
-        this.setState({
-            isOpen: !this.state.isOpen
-        });
-    }
+  toggle = () => {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  };
 
-    render() {
-        const { isAuthenticated, user } = this.props.auth;
+  render() {
+    const { isAuthenticated, user } = this.props.auth;
 
-        const authLinks = (
-            <Fragment>
-                <NavItem>
-                    <span className="navbar-text mr-3">
-                        <strong>{ user ? `Welcome ${user.name}` : '' }</strong>
-                    </span>
-                </NavItem>
-                <NavItem>
-                    <Logout />
-                </NavItem>
-            </Fragment>
-        )
+    const authLinks = (
+      <Fragment>
+        <NavItem>
+          <span className="navbar-text mr-3">
+            <strong>{user ? `Welcome ${user.name}` : ''}</strong>
+          </span>
+        </NavItem>
+        <NavItem>
+          <Logout />
+        </NavItem>
+      </Fragment>
+    );
 
-        const guestLinks = (
-            <Fragment>
-                <NavItem>
-                    <RegisterModal />
-                    {/* <NavLink href="https://github.com/ZhouYou528/ReactShoppingList">Github</NavLink> */}
-                </NavItem>
-                <NavItem>
-                    <Login />
-                </NavItem>
-            </Fragment>
-        )
+    const guestLinks = (
+      <Fragment>
+        <NavItem>
+          <RegisterModal />
+          {/* <NavLink href="https://github.com/ZhouYou528/ReactShoppingList">Github</NavLink> */}
+        </NavItem>
+        <NavItem>
+          <Login />
+        </NavItem>
+      </Fragment>
+    );
 
-        return (
-            <div>
-                <Navbar color="dark" dark expand="sm" className="mb-5">
-                    <Container>
-                        <NavbarBrand href="/">Shopping List</NavbarBrand>
-                        <NavbarToggler onClick={this.toggle} />
-                        <Collapse isOpen={this.state.isOpen} navbar>
-                            <Nav className="ml-auto" navbar>
-                                { isAuthenticated ? authLinks : guestLinks }
-                            </Nav>
-                        </Collapse>
-                    </Container>
-                </Navbar>
-            </div>
-        );
-    }
+    return (
+      <div>
+        <Navbar color="dark" dark expand="sm" className="mb-5">
+          <Container>
+            <NavbarBrand href="/">Shopping List</NavbarBrand>
+            <NavbarToggler onClick={this.toggle} />
+            <Collapse isOpen={this.state.isOpen} navbar>
+              <Nav className="ml-auto" navbar>
+                {isAuthenticated ? authLinks : guestLinks}
+              </Nav>
+            </Collapse>
+          </Container>
+        </Navbar>
+      </div>
+    );
+  }
 }
 
 const mapStateToProps = state => ({
-    auth: state.auth
+  auth: state.auth
 });
 
-export default connect(mapStateToProps, null)(AppNavbar);
+export default connect(
+  mapStateToProps,
+  { loadUser }
+)(AppNavbar);
--- client/src/components/itemModal.js
+++ client/src/components/itemModal.js
@@ -1,88 +1,116 @@
 import React, { Component } from 'react';
 import {
-    Button,
-    Modal,
-    ModalHeader,
-    ModalBody,
-    Form,
-    FormGroup,
-    Label,
-    Input
+  Button,
+  Modal,
+  ModalHeader,
+  ModalBody,
+  Form,
+  FormGroup,
+  Label,
+  Input,
+  Alert
 } from 'reactstrap';
 import { connect } from 'react-redux';
 import { addItem } from '../actions/itemActions';
+import { clearErrors } from '../actions/errorAction';
+import PropTypes from 'prop-types';
 
 class ItemModal extends Component {
-    state = {
-        modal: false,
-        name: ''
-    }
+  //   constructor(props) {
+  //     super(props);
+  //     this.onSubmit = this.onSubmit.bind(this);
+  //   }
+  state = {
+    modal: false,
+    name: '',
+    msg: null
+  };
 
-    toggle = () => {
-        this.setState({
-            modal: !this.state.modal
-        });
-    }
+  static propTypes = {
+    error: PropTypes.object.isRequired,
+    addItem: PropTypes.func.isRequired,
+    clearErrors: PropTypes.func.isRequired
+  };
 
-    onChange = e => {
-        this.setState({[e.target.name]: e.target.value});
+  componentDidUpdate(prevProps) {
+    const { error } = this.props;
+    if (error !== prevProps.error) {
+      //check for register error
+      if (error.id === 'ADD_FAIL') {
+        this.setState({ msg: error.msg.msg });
+      } else {
+        this.setState({ msg: null });
+      }
     }
-
-    onSubmit = e => {
-        e.preventDefault();
-        const newItem = {
-            name: this.state.name
-        }
-        //Add item via addItem action
-        this.props.addItem(newItem);
-        //Close Modal
-        this.toggle();
+    if (this.state.added && this.props.modal) {
+      console.log('123');
+      if (!error.msg.hasOwnProperty('msg')) this.toggle();
     }
+  }
+  toggle = () => {
+    this.props.clearErrors();
+    this.setState({
+      modal: !this.state.modal
+    });
+  };
 
-    render() {
-        return (
-            <div>
-                <Button
-                    color="dark"
-                    style={{marginBottom: '2rem'}}
-                    onClick={this.toggle}
-                >
-                    Add Item
+  onChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  onSubmit = async e => {
+    e.preventDefault();
+    const newItem = {
+      name: this.state.name
+    };
+    //Add item via addItem action
+    this.props.addItem(newItem);
+  };
+
+  render() {
+    return (
+      <div>
+        <Button
+          color="dark"
+          style={{ marginBottom: '2rem' }}
+          onClick={this.toggle}
+        >
+          Add Item
+        </Button>
+        <Modal isOpen={this.state.modal} toggle={this.toggle}>
+          <ModalHeader toggle={this.toggle}>Add to Shopping List</ModalHeader>
+          <ModalBody>
+            {this.state.msg ? (
+              <Alert color="danger">{this.state.msg}</Alert>
+            ) : null}
+            <Form onSubmit={this.onSubmit}>
+              <FormGroup>
+                <Label for="item">Item</Label>
+                <Input
+                  type="text"
+                  name="name"
+                  id="item"
+                  placeholder="Add shopping item"
+                  onChange={this.onChange}
+                />
+                <Button color="dark" style={{ marginTop: '2rem' }} block>
+                  Add Item
                 </Button>
-                <Modal
-                    isOpen={this.state.modal}
-                    toggle={this.toggle}
-                >
-                    <ModalHeader toggle={this.toggle}>
-                        Add to Shopping List
-                    </ModalHeader>
-                    <ModalBody>
-                        <Form onSubmit={this.onSubmit}>
-                            <FormGroup>
-                                <Label for="item">Item</Label>
-                                <Input 
-                                    type="text"
-                                    name="name"
-                                    id="item"
-                                    placeholder="Add shopping item"
-                                    onChange={this.onChange}
-                                ></Input>
-                                <Button
-                                    color="dark"
-                                    style={{marginTop: '2rem'}}   
-                                    block 
-                                >Add Item</Button>
-                            </FormGroup>
-                        </Form>
-                    </ModalBody>
-                </Modal>
-            </div>
-        );
-    }
+              </FormGroup>
+            </Form>
+          </ModalBody>
+        </Modal>
+      </div>
+    );
+  }
 }
 
 const mapStateToProps = state => ({
-    item: state.item
+  item: state.item,
+  error: state.error
 });
 
-export default connect(mapStateToProps, { addItem })(ItemModal);
+export default connect(
+  mapStateToProps,
+  { addItem, clearErrors }
+)(ItemModal);
